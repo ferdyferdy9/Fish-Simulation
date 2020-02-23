5571bc8031a21f5d82e3e869d76d3d3656d4fc86
ferdyferdy9 <ferdy.nicolas@gmail.com>
2020-02-23 15:28:59 +0700

    2DBoids Model B

---
+++ Scenes/2D_Boids_Model_B/Boid2D.gd
@@ -0,0 +1,176 @@
+extends RigidBody2D
+
+class_name Boid2D
+
+export var max_speed:float = 800
+export var max_force:float = 80
+export var vision_dist: float = 300
+export var vision_radius: float = 240
+export var debug_mode: bool = false
+
+var window_width = ProjectSettings.get_setting("display/window/size/width")
+var window_height = ProjectSettings.get_setting("display/window/size/height")
+var friends
+
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	$Sprite.rotation = atan2(linear_velocity.y, linear_velocity.x)
+	
+	if debug_mode:
+		update()
+
+
+func _integrate_forces(state):
+	if position.x < -50:
+		state.transform = Transform2D(0, Vector2(window_width+50, state.transform.get_origin().y))
+		position.x = window_width+50
+	if position.x > window_width+50:
+		state.transform = Transform2D(0, Vector2(-50, state.transform.get_origin().y))
+		position.x = -50
+	if position.y < -50:
+		state.transform = Transform2D(0, Vector2(state.transform.get_origin().x, window_height+50))
+		position.y = window_height+50
+	if position.y > window_height+50:
+		state.transform = Transform2D(0, Vector2(state.transform.get_origin().x, -50))
+		position.y = -50
+
+
+func _physics_process(delta):
+	if linear_velocity.length() > max_speed:
+		linear_velocity = linear_velocity.normalized() * max_speed
+	
+	var forces: Array = []
+	forces.append(0.1*keep_going())
+	forces.append(seperate(friends))
+	forces.append(align(friends))
+	forces.append(cohesion(friends))
+	
+	apply_force(forces)
+
+
+func apply_force(forces):
+	var total: Vector2
+	
+	for f in forces:
+		total += f
+	total /= forces.size()
+	
+	linear_velocity += total
+	
+
+func checkVisionTrigger(target: Vector2, dist: float) -> bool:
+	var angle = atan2(target.y - position.y, target.x - position.x)
+	return ((position - target).length() < dist and abs(angle-rotation) < deg2rad(vision_radius*0.5))
+
+
+func keep_going():
+	var desire := linear_velocity.normalized() * max_speed
+	var force := desire - linear_velocity
+	if force.length_squared() > max_force*max_force:
+		force = force.normalized() * max_force
+	
+	return force
+
+
+func seek(target: Vector2):
+	var desired := target - position
+	desired = desired.normalized() * max_speed
+	
+	var force := desired - linear_velocity
+	if force.length() > max_force:
+		force = force.normalized() * max_force
+	
+	return force
+
+
+func seperate(group: Array):
+	var total_desire: Vector2
+	var count = 0
+	
+	for f in group:
+		if f == self:
+			continue
+		
+		if checkVisionTrigger(f.position, vision_dist*0.25):
+			var desired = position - f.position
+			total_desire += desired.normalized()
+			count += 1
+	
+	var force: Vector2
+	
+	if count > 0:
+		force = total_desire*max_speed/count
+		if force.length_squared() > max_force*max_force:
+			force = force.normalized() * max_force
+	
+	return force
+
+
+func align(group: Array):
+	var total_desire: Vector2
+	var count = 0
+	
+	for f in group:
+		if f == self:
+			continue
+		
+		if checkVisionTrigger(f.position, vision_dist):
+			total_desire += f.linear_velocity.normalized()
+			count += 1
+	
+	var force: Vector2
+	
+	if count > 0:
+		force = total_desire*max_speed/count - linear_velocity
+		if force.length_squared() > max_force*max_force:
+			force = force.normalized() * max_force
+	
+	return force
+
+
+func cohesion(group: Array):
+	var desired_pos: Vector2
+	var count = 0
+	
+	for f in group:
+		if f == self:
+			continue
+		
+		if checkVisionTrigger(f.position, vision_dist*1.2):
+			desired_pos += f.position
+			count += 1
+	
+	var force: Vector2
+	
+	if count > 0:
+		force = seek(desired_pos/count)
+		if force.length_squared() > max_force*max_force:
+			force = force.normalized() * max_force
+	
+	return force
+
+
+func _draw():
+	if debug_mode:
+		draw_line(Vector2(), linear_velocity*0.25, Color.white)
+		draw_circle_arc_poly(
+			Vector2(), vision_dist, 
+			$Sprite.rotation_degrees+90-vision_radius*0.5, 
+			$Sprite.rotation_degrees+90+vision_radius*0.5,
+			Color(1,1,1,0.1))
+
+
+func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
+	var nb_points = 32
+	var points_arc = PoolVector2Array()
+	points_arc.push_back(center)
+	var colors = PoolColorArray([color])
+
+	for i in range(nb_points + 1):
+		var angle_point = deg2rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
+		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
+	draw_polygon(points_arc, colors)
--- Scenes/2D_Boids_Model_B/Main.gd
+++ Scenes/2D_Boids_Model_B/Main.gd
@@ -7,30 +7,38 @@ var window_width = ProjectSettings.get_setting("display/window/size/width")
 var window_height = ProjectSettings.get_setting("display/window/size/height")
 
 func _ready():
-	randomize()
-	for _i in range(60):
+	for _i in range(50):
 		var b = boid_prototype.instance()
 		
 		b.position = Vector2(rand_range(0,window_width), rand_range(0,window_height))
+		b.linear_velocity = Vector2(100,10)
 		b.linear_velocity = Vector2.ONE.rotated(rand_range(0, PI*2)) * 100
 		
 		add_child(b)
 		boids.append(b)
-	
-	boids[0].debug_mode = true
 	
 	for b in boids:
 		b.friends = boids
 
 
 func _physics_process(delta):
+	for b in boids:
+		if b.position.x < -50:
+			b.position.x = window_width+50
+		if b.position.x > window_width+50:
+			b.position.x = -50
+		if b.position.y < -50:
+			b.position.y = window_height+50
+		if b.position.y > window_height+50:
+			b.position.y = -50
+	
 	if Input.is_key_pressed(KEY_SPACE):
 		get_tree().reload_current_scene()
 	
 	if Input.is_key_pressed(KEY_RIGHT):
 		for b in boids:
-			b.vision_radius = clamp(b.vision_radius+1, 0, 360)
+			b.vision_radius += 1
 			
 	if Input.is_key_pressed(KEY_LEFT):
 		for b in boids:
-			b.vision_radius = clamp(b.vision_radius-1, 0, 360)
+			b.vision_radius -= 1
